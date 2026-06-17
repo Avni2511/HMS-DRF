c5e3e0ed2035a8c89cc2db7bc0f626db780a0989
Avni2511 <avnigupta2505@gmail.com>
2026-06-17 11:41:23 +0530

    Configure PostgreSQL

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 
@@ -87,12 +88,10 @@ WSGI_APPLICATION = 'Hospital.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
