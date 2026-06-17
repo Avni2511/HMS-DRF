836623fc96c8821ae0e09188e0291b32128362bf
Avni2511 <avnigupta2505@gmail.com>
2026-06-17 12:08:27 +0530

    Fix database configuration

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -87,11 +87,20 @@ WSGI_APPLICATION = 'Hospital.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
-}
+
+if os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.getenv("DATABASE_URL")
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
